54cdcf6086b8f65bd65971c4a65fa97a774f7c37
Shashwat Pandey <47330116+shashwattpandeyy@users.noreply.github.com>
2020-10-14 13:33:55 +0530

    update app page

--- app/src/main/java/com/example/stockmoney/Login.kt
+++ app/src/main/java/com/example/stockmoney/Login.kt
@@ -1,11 +1,18 @@
 package com.example.stockmoney
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun loginPage(view: View) {
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
     }
 }
